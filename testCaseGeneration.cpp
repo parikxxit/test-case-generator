d97bda5af3fa0fda4230c98a352423abfc70eb61
Parikshit <singhp154154@gmail.com>
2019-02-19 21:15:42 +0530

    Made baic boiler plate

--- testCaseGeneration.cpp
+++ testCaseGeneration.cpp
@@ -2,16 +2,33 @@
 using namespace std;
 int main() {
 	int t,n;
-	cout << "Enter no of test case ";
+	cout << "Enter no of test case (enter 1 if there is no test case)"; 
 	cin >> t;
+	int case_;
+	cout << "Enter 1 for generating random number." << endl;
+	cout << "Enter 2 for generating random array,matrix,pair." << endl;
+	cout << "Enter 3 for generating random strings or array of strings " << endl;
+	cin >> case_;
 	switch(case_) {
 		case 1 :
+			int min,max;
+			cout << "Enter Min value of integer ";
+			cin >> min;
+			cout << endl << "Enter Max value of integer ";
+			cin >> max;
+			cout << endl;
 			break;
 		case 2 :
+			int arr_ch;
+			cout << "1 for random array \n2 for random array of pairs.\n3 for random matrix ";
+			cin >> arr_ch;
+			cout << endl;
 			break;
 		case 3 :
-			break;
-		case 4 :
+			int string_ch;
+			cout << "1 for random string \n2 for ramdom character matrix ";
+			cin >> string_ch;
+			cout << endl;
 			break;
 		default :
 			cout << "Wrong ip try again" << endl;
