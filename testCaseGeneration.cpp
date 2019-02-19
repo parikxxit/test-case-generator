5f971f6a4c0bc1c9af19ab4ebbb4f57052edb3d7
Parikshit Singh <singhp154154@gmail.com>
2019-02-19 20:38:51 +0530

    Added main file

---
+++ testCaseGeneration.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int t,n;
+	cout << "Enter no of test case ";
+	cin >> t;
+	switch(case_) {
+		case 1 :
+			break;
+		case 2 :
+			break;
+		case 3 :
+			break;
+		case 4 :
+			break;
+		default :
+			cout << "Wrong ip try again" << endl;
+			break;
+	} 
+	return 0;
+}
